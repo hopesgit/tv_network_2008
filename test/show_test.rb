045f5e36b59a8fd84e127e748392d6a86ea1958b
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-01 09:14:35 -0600

    Create Show class

---
+++ test/show_test.rb
@@ -0,0 +1,13 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/show"
+
+class ShowTest < Minitest::Test
+  def test_it_is_show
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_instance_of Show, knight_rider
+  end
+end
