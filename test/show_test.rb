35bc90543b41e5f133d86b0da3f44a9323401525
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-01 09:27:14 -0600

    Add Show actors method

--- test/show_test.rb
+++ test/show_test.rb
@@ -36,4 +36,12 @@ class ShowTest < Minitest::Test
 
     assert_equal "David Hasselhoff", knight_rider.highest_paid_actor
   end
+
+  def test_it_can_get_actors
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal ["David Hasselhoff", "William Daniels"], knight_rider.actors
+  end
 end
